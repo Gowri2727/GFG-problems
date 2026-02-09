8fa94deb95c17974def2984cb839515df6c50967
Gowri Shankar <22A91A1207@aec.edu.in>
2026-02-09 17:08:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factors Sum/factors-sum.py
@@ -0,0 +1,14 @@
+#User function Template for python3
+
+class Solution:
+    def factorSum(self, N):
+        # code here
+        total=0
+        i=1
+        while i*i<=N:
+            if N%i==0:
+                total+=i
+                if i!=N//i:
+                    total +=N//i
+            i+=1
+        return total
